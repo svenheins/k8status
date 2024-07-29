f6f4589605c445ff4701517a0bb9557df08f70ea
Sven Heins <svenheinswork@gmail.com>
2024-07-29 13:00:22 +0200

    delete deployment and ingress

---
+++ 04_delete_deployment_ingress.sh
@@ -0,0 +1,25 @@
+export $(cat .env)
+
+echo 'the deployment file looks like this: '
+envsubst < deployment.yaml | cat -
+
+# Prompt the user
+read -p "Do you want to proceed DELETING the deployment and ingress? (y/n): " choice
+
+# Process the response
+case "$choice" in 
+  y|Y ) 
+    echo "Proceeding..."
+    # Add your command here
+    echo "kubectl delete -f deployment.yaml && kubectl delete -f ingress.yaml"
+    envsubst < deployment.yaml | kubectl delete -f - \
+      && envsubst < ingress.yaml | kubectl delete -f -
+    ;;
+  n|N ) 
+    echo "Aborting..."
+    ;;
+  * ) 
+    echo "Invalid input - use y or n"
+    ;;
+esac
+
