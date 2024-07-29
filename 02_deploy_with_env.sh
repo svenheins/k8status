c4b5ff8cf6ea5ee97e270419e2cf864ac9de3530
Sven Heins <svenheinswork@gmail.com>
2024-07-29 11:12:56 +0200

    improved env processing

--- 02_deploy_with_env.sh
+++ 02_deploy_with_env.sh
@@ -1,1 +1,24 @@
-envsubst < deployment.yaml | kubectl apply -f -
+export $(cat .env)
+
+echo 'the deployment file looks like this: '
+envsubst < deployment.yaml | cat -
+
+# Prompt the user
+read -p "Do you want to proceed? (y/n): " choice
+
+# Process the response
+case "$choice" in 
+  y|Y ) 
+    echo "Proceeding..."
+    # Add your command here
+    echo "kubectl apply -f deployment.yaml"
+    envsubst < deployment.yaml | kubectl apply -f -
+    ;;
+  n|N ) 
+    echo "Aborting..."
+    ;;
+  * ) 
+    echo "Invalid input - use y or n"
+    ;;
+esac
+
