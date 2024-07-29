2f71685352fb62b32b9bb2ec5e201332e2e99941
Sven Heins <svenheinswork@gmail.com>
2024-07-29 11:17:30 +0200

    improve env for ingress

--- 03_ingress_with_env.sh
+++ 03_ingress_with_env.sh
@@ -1,1 +1,23 @@
-envsubst < ingress.yaml | kubectl apply -f -
+export $(cat .env)
+
+echo 'the ingress file looks like this: '
+envsubst < ingress.yaml | cat -
+
+# Prompt the user
+read -p "Do you want to proceed? (y/n): " choice
+
+# Process the response
+case "$choice" in 
+  y|Y ) 
+    echo "Proceeding..."
+    # Add your command here
+    echo "kubectl apply -f ingress.yaml"
+    envsubst < ingress.yaml | kubectl apply -f -
+    ;;
+  n|N ) 
+    echo "Aborting..."
+    ;;
+  * ) 
+    echo "Invalid input - use y or n"
+    ;;
+esac
